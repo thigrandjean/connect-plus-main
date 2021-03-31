b2b288f3991456db9e3f61181b114b59421775e3
Thiago Grandjean <thigrandjean@yahoo.com.br>
2021-03-30 21:33:21 -0300

    4 contact/locations tests

---
+++ src/pages/Contact2/Locations.vue
@@ -0,0 +1,27 @@
+<template>
+  <Layout>
+    <BannerPageTopLocations />
+    <SearchLocations />
+  </Layout>
+</template>
+
+
+<script>
+import BannersPageFooter from '@/components/BannersPageFooter.vue'
+import SearchLocations from '@/components/SearchLocations.vue'
+import BannerPageTopLocations from '@/components/BannerPageTopLocations.vue'
+
+export default {
+  components: { BannersPageFooter, SearchLocations, BannerPageTopLocations },
+
+  metaInfo() {
+    return {
+      title: this.$t('ourlocations.title'),
+      meta: [
+        { name: 'description', content: this.$t('ourlocations.description') },
+      ],
+    }
+  },
+}
+</script>
+
---
+++ src/pages/Contact4/Locationsnomap.vue
@@ -0,0 +1,79 @@
+<template>
+  <Layout>
+    <div class="pages-banner">
+      <h1>{{ $t('ourlocations.title') }}</h1>
+      <h2>{{ $t('ourlocations.description') }}</h2>
+
+      <div class="image-wrap">
+        <g-image
+          src="@/assets/images/banner-our-locations.jpg"
+          :alt="$t('ourlocations.title')"
+        />
+      </div>
+    </div>
+
+    <SearchLocations nomap />
+  </Layout>
+</template>
+
+
+<script>
+import BannersPageFooter from '@/components/BannersPageFooter.vue'
+import SearchLocations from '@/components/SearchLocations.vue'
+
+export default {
+  components: { BannersPageFooter, SearchLocations },
+
+  metaInfo() {
+    return {
+      title: this.$t('ourlocations.title'),
+      meta: [
+        { name: 'description', content: this.$t('ourlocations.description') },
+      ],
+    }
+  },
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '~/assets/style/vars.scss';
+.pages-banner {
+  height: 10.5rem;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 1rem;
+  position: relative;
+  overflow: hidden;
+  @media (min-width: $bp-mobile) {
+    height: 16.785rem;
+  }
+  h1 {
+    color: white;
+    margin: 0;
+    text-align: center;
+    font-weight: normal;
+    z-index: 3;
+    font-size: 1.428rem;
+    @media (min-width: $bp-mobile) {
+      font-size: 3.428rem;
+    }
+  }
+  h2 {
+    color: white;
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+    font-weight: normal;
+    z-index: 3;
+    font-size: 1.142rem;
+    font-weight: 300;
+    @media (min-width: $bp-mobile) {
+      font-size: 1.71rem;
+    }
+  }
+}
+</style>
